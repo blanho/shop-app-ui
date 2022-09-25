7b3bafec45b06c374885ae4aa22f025797233957
blanho <h.baolan20025@gmail.com>
2022-09-25 08:53:27 +0700

    Fetch Products

--- src/context/product_context.js
+++ src/context/product_context.js
@@ -1,22 +1,54 @@
-import React, { useContext, useReducer } from "react";
+import React, { useContext, useEffect, useReducer } from "react";
 import reducer from "../reducers/product_reducer";
+import axios from "axios";
+import {
+  SIDEBAR_CLOSE,
+  SIDEBAR_OPEN,
+  GET_PRODUCTS_START,
+  GET_PRODUCTS_ERROR,
+  GET_PRODUCTS_SUCCESS,
+} from "../actions";
+import { products_url as url } from "../utils/constants";
 
-import { SIDEBAR_CLOSE, SIDEBAR_OPEN } from "../actions";
 const ProductContext = React.createContext();
 
 const initialState = {
   isSidebarOpen: false,
+  products_loading: false,
+  products_error: false,
+  products: [],
+  featured_products: [],
 };
 
 export const ProductProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  // Sidebar
   const openSidebar = () => {
     dispatch({ type: SIDEBAR_OPEN });
   };
+
   const closeSidebar = () => {
     dispatch({ type: SIDEBAR_CLOSE });
   };
+
+  // Fetch Products
+  const fetchProducts = async (url) => {
+    dispatch({ type: GET_PRODUCTS_START });
+    try {
+      const res = await axios.get(url);
+      const products = res.data;
+
+      dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products });
+    } catch (error) {
+      dispatch({ type: GET_PRODUCTS_ERROR });
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts(url);
+  }, []);
+
   return (
     <ProductContext.Provider value={{ ...state, openSidebar, closeSidebar }}>
       {children}
--- src/reducers/product_reducer.js
+++ src/reducers/product_reducer.js
@@ -1,13 +1,39 @@
-import { SIDEBAR_OPEN, SIDEBAR_CLOSE } from "../actions";
+import {
+  SIDEBAR_OPEN,
+  SIDEBAR_CLOSE,
+  GET_PRODUCTS_ERROR,
+  GET_PRODUCTS_START,
+  GET_PRODUCTS_SUCCESS,
+} from "../actions";
 
 const product_reducer = (state, action) => {
   if (action.type === SIDEBAR_OPEN) {
     return { ...state, isSidebarOpen: true };
   }
+
   if (action.type === SIDEBAR_CLOSE) {
     return { ...state, isSidebarOpen: false };
   }
-  return state;
+
+  if (action.type === GET_PRODUCTS_START)
+    return { ...state, products_loading: true };
+
+  if (action.type === GET_PRODUCTS_SUCCESS) {
+    const featured_products = action.payload.filter(
+      (product) => product.featured === true
+    );
+
+    return {
+      ...state,
+      products_loading: false,
+      featured_products,
+      products: action.payload,
+    };
+  }
+  if (action.type === GET_PRODUCTS_ERROR) {
+    return { ...state, products_loading: false, products_error: true };
+  }
+  throw new Error(`No matching ${action.type} - action type`);
 };
 
 export default product_reducer;
