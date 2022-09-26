9eb0df946e36b8fd70a432c615fdbd00fc8612ba
blanho <h.baolan20025@gmail.com>
2022-09-26 19:39:07 +0700

    Filters - Set Default Price and Max-Price

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -8,10 +8,15 @@ import {
 
 const filter_reducer = (state, action) => {
   if (action.type === LOAD_PRODUCTS) {
+    // Using the spread operator retrieves max Price and min Price as well
+    let maxPrice = action.payload.map((product) => product.price);
+    maxPrice = Math.max(...maxPrice);
+
     return {
       ...state,
       all_products: [...action.payload],
       filtered_products: [...action.payload],
+      filters: { ...state.filters, max_price: maxPrice, price: maxPrice },
     };
   }
   if (action.type === GRID_VIEW) {
