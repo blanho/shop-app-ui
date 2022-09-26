500c58ebffe1c2284f47fb633eca623653f3a6db
blanho <h.baolan20025@gmail.com>
2022-09-27 01:33:50 +0700

    Filter - Companies

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -62,6 +62,24 @@ function Filters() {
               })}
             </div>
           </div>
+          {/* companies */}
+          <div className="form-control">
+            <h5>company</h5>
+            <select
+              name="company"
+              value={company}
+              onChange={updateFilters}
+              className="company"
+            >
+              {companies.map((c, index) => {
+                return (
+                  <option key={index} value={c}>
+                    {c}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
         </form>
       </div>
     </Wrapper>
