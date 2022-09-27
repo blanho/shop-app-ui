16733f00d6d618ffcc3a18b5d6f161a575fd2206
blanho <h.baolan20025@gmail.com>
2022-09-27 16:19:54 +0700

    Cart - Cart Context Setup

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -1,11 +1,20 @@
-import React from "react";
-import { useContext } from "react";
+import React, { useReducer, useContext } from "react";
+
+import reducer from "../reducers/cart_reducer";
+
+const initialState = {
+  cart: [],
+  total_items: 0,
+  total_amount: 0,
+  shipping_fee: 534,
+};
 
 const CartContext = React.createContext();
 
 export const CartProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
-    <CartContext.Provider value="cart context">{children}</CartContext.Provider>
+    <CartContext.Provider value={{ ...state }}>{children}</CartContext.Provider>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App";
 import { ProductProvider } from "./context/product_context";
 import { FilterProvider } from "./context/filter_context";
+import { CartProvider } from "./context/cart_context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ProductProvider>
       <FilterProvider>
-        <App />
+        <CartProvider>
+          <App />
+        </CartProvider>
       </FilterProvider>
     </ProductProvider>
   </React.StrictMode>
