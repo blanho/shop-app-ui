4f1857fa9e5d6c0f4e7865f1bfa4288d3ca36955
blanho <h.baolan20025@gmail.com>
2022-09-29 09:01:01 +0700

    Cart - Cart Item

--- src/components/CartContent.jsx
+++ src/components/CartContent.jsx
@@ -1,7 +1,58 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { useCartContext } from "../context/cart_context";
+import CartItem from "./CartItem";
+import CartTotal from "./CartTotal";
+import CartColumn from "./CartColumn";
 
 function CartContent() {
-  return <div>CartContent</div>;
+  const { cart, clearCart } = useCartContext();
+  return (
+    <Wrapper className="section section-center">
+      <CartColumn />
+      {cart.map((item) => {
+        return <CartItem key={item.id} {...item} />;
+      })}
+      <hr />
+      <div className="link-container">
+        <Link to="/products" className="link-btn">
+          continue shopping
+        </Link>
+        <button
+          type="button"
+          className="link-btn clear-btn"
+          onClick={clearCart}
+        >
+          clear shopping cart
+        </button>
+      </div>
+      <CartTotal />
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.section`
+  .link-container {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 2rem;
+  }
+  .link-btn {
+    background: transparent;
+    border-color: transparent;
+    text-transform: capitalize;
+    padding: 0.25rem 0.5rem;
+    background: var(--clr-primary-5);
+    color: var(--clr-white);
+    letter-spacing: var(--spacing);
+    border-radius: var(--radius);
+    font-weight: 400;
+    cursor: pointer;
+  }
+  .clear-btn {
+    background-color: var(--clr-black);
+  }
+`;
 
 export default CartContent;
